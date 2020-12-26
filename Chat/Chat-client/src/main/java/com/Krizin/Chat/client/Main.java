0ddfe3e43b4dc60562b40d4c4d65715b7f41bc32
Дик Александр <67008078+DisKass@users.noreply.github.com>
2020-12-26 10:25:30 +0300

    Все файлы курсовой работы + отчет

---
+++ Chat/Chat-client/src/main/java/com/Krizin/Chat/client/Main.java
@@ -0,0 +1,26 @@
+package com.Krizin.Chat.client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/client.fxml"));
+        Parent root = fxmlLoader.load();
+        Controller controller = fxmlLoader.getController();
+        primaryStage.setOnCloseRequest(event -> controller.exitAction());
+        primaryStage.setTitle("My Chat Client");
+        primaryStage.setScene(new Scene(root, 800, 500));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
